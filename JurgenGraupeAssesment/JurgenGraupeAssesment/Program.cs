5208f1045ca70274157c825766603d7996cf3fa6
JurgenGraupe <graupe.jurgen@gmail.com>
2023-02-13 15:21:48 +0200

    Updated efficiency
    Made binary reader more efficient, parallelised foreach

--- JurgenGraupeAssesment/JurgenGraupeAssesment/Program.cs
+++ JurgenGraupeAssesment/JurgenGraupeAssesment/Program.cs
@@ -1,15 +1,12 @@
-﻿using System.Text;
-using JurgenGraupeAssesment;
+﻿using JurgenGraupeAssesment;
 using NetTopologySuite.Geometries;
 using NetTopologySuite.Index.KdTree;
+using System.Text;
 
-class Program {    
-    
+class Program {
     static void Main(string[] args) {
-        // Read binary data file
-        var kdTree = ReadBinaryDataFile("VehiclePositions.dat");        
-
-        // Define the 10 coordinates to look up
+        var kdTree = ReadBinaryDataFile("VehiclePositions.dat");
+        
         var coordinates = new Coordinate[] {
             new Coordinate(y: 34.544909, x: -102.100843),
             new Coordinate(y: 32.345544, x: -99.123124),
@@ -20,44 +17,56 @@ class Program {
             new Coordinate(y: 34.115839, x: -100.225732),
             new Coordinate(y: 32.335839, x: -99.992232),
             new Coordinate(y: 33.535339, x: -94.792232),
-            new Coordinate(y: 32.234235, x: -100.222222)            
+            new Coordinate(y: 32.234235, x: -100.222222)
         };
-
-        // Find nearest vehicle position for each coordinate        
-        foreach (var coord in coordinates) {            
-            var nearest = kdTree.NearestNeighbor(coord);            
+        
+        Parallel.ForEach(coordinates, coord =>  {
+            var nearest = kdTree.NearestNeighbor(coord);
             Console.WriteLine($"Nearest vehicle to {coord.X}, {coord.Y}: \nPositionId: {nearest.Data.PositionId}\nRegistration: {nearest.Data.VehicleRegistration}\nLongitude: {nearest.Data.Longitude}\nLatitude: {nearest.Data.Latitude}\nTime: {nearest.Data.RecordedTimeUTC}\n\n");
-        }        
+        });
     }
 
     public static KdTree<Vehicle> ReadBinaryDataFile(string filePath) {
-        using (var reader = new BinaryReader(File.OpenRead(filePath))) {
-            var kdTree = new KdTree<Vehicle>();
-            while (reader.BaseStream.Position < reader.BaseStream.Length) {
-                var positionId = reader.ReadInt32();
-                var vehicleRegistration = ReadNullTerminatedString(reader);
-                var latitude = reader.ReadSingle();
-                var longitude = reader.ReadSingle();
-                var recordedTimeUTC = reader.ReadUInt64();
-                var vehicle = new Vehicle() {
-                    PositionId = positionId,
-                    VehicleRegistration = vehicleRegistration,
-                    Latitude = latitude,
-                    Longitude = longitude,
-                    RecordedTimeUTC = recordedTimeUTC
-                };
-                kdTree.Insert(new Coordinate(vehicle.Longitude, vehicle.Latitude), vehicle);
+        var kdTree = new KdTree<Vehicle>();
+        using (var fileStream = new FileStream(filePath, FileMode.Open, FileAccess.Read))
+        using (var reader = new BinaryReader(new BufferedStream(fileStream))) {
+            int chunkSize = 4096;
+            var buffer = new byte[chunkSize];
+            int bytesRead;
+            while ((bytesRead = reader.Read(buffer, 0, buffer.Length)) > 0) {
+                using (var stream = new MemoryStream(buffer, 0, bytesRead))
+                using (var binaryReader = new BinaryReader(stream)) {
+                    try {
+                        while (stream.Position < stream.Length) {
+                            var positionId = binaryReader.ReadInt32();
+                            var vehicleRegistration = ReadNullTerminatedString(binaryReader);
+                            var latitude = binaryReader.ReadSingle();
+                            var longitude = binaryReader.ReadSingle();
+                            var recordedTimeUTC = binaryReader.ReadUInt64();
+                            var vehicle = new Vehicle() {
+                                PositionId = positionId,
+                                VehicleRegistration = vehicleRegistration,
+                                Latitude = latitude,
+                                Longitude = longitude,
+                                RecordedTimeUTC = recordedTimeUTC
+                            };
+                            kdTree.Insert(new Coordinate(vehicle.Longitude, vehicle.Latitude), vehicle);
+                        }
+                    } catch (EndOfStreamException) {
+                        break;
+                    }
+                }
             }
-            return kdTree;
         }
+        return kdTree;
     }
 
-    public static string ReadNullTerminatedString(BinaryReader reader) {
-        var bytes = new List<byte>();
+    private static string ReadNullTerminatedString(BinaryReader reader) {
+        List<byte> stringBytes = new List<byte>();
         byte b;
         while ((b = reader.ReadByte()) != 0) {
-            bytes.Add(b);
+            stringBytes.Add(b);
         }
-        return Encoding.ASCII.GetString(bytes.ToArray());
+        return Encoding.UTF8.GetString(stringBytes.ToArray());
     }    
 }
--- JurgenGraupeAssesment/JurgenGraupeAssesment/Vehicle.cs
+++ JurgenGraupeAssesment/JurgenGraupeAssesment/Vehicle.cs
@@ -1,9 +1,9 @@
-﻿namespace JurgenGraupeAssesment {    
-    public class Vehicle {        
-        public int PositionId { get; set; }        
+﻿namespace JurgenGraupeAssesment {
+    public class Vehicle {
+        public int PositionId { get; set; }
         public string VehicleRegistration { get; set; }
         public float Latitude { get; set; }
         public float Longitude { get; set; }
         public ulong RecordedTimeUTC { get; set; }
-    }    
+    }
 }
